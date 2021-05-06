0569c317629a03499d812a3cd9665fdce0f202dc
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2021-05-06 22:57:52 +0300

    Create rw_visual.py
    plot page of project

---
+++ rw_visual.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+# Make a random walk.
+rw = RandomWalk()
+rw.fill_walk()
+
+# Plot the points in the walk.
+plt.style.use('classic')
+fig, ax = plt.subplots()
+ax.scatter(rw.x_values, rw.y_values, s=15)
+plt.show()
